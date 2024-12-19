6a8f7fcd4467f454dced7baff2c8c818fa52f041
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-12-19 08:06:30 +0000

    Refactor Home Movies Mapped

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -13,32 +13,33 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<div>
-				{/* {store.movies?.map((movie, index) => {
-					<div key={index} >
+				{store.movies?.results?.map((movie, index) => (
+					<div key={index} className="movie-card">
 						<h3>{movie.title}</h3>
 						<p>{movie.overview}</p>
-						<img 
-							src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-							alt={movie.title} 
-							style={{ width: "200px", height: "300px" }} 
+						<img
+							src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+							alt={movie.title}
+							style={{ width: "200px", height: "300px" }}
 						/>
 					</div>
-				})} */}
-				{store.movies?.results?.length ? (
+				))}
+
+				{/* {store.movies?.results?.length ? (
 					store.movies.results.map((movie, index) => (
 						<div key={index} className="movie-card">
 							<h3>{movie.title}</h3>
 							<p>{movie.overview}</p>
-							<img 
-								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-								alt={movie.title} 
-								style={{ width: "200px", height: "300px" }} 
+							<img
+								src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+								alt={movie.title}
+								style={{ width: "200px", height: "300px" }}
 							/>
 						</div>
 					))
 				) : (
 					<p>Loading movies...</p>
-				)}
+				)} */}
 			</div>
 		</div>
 	);
